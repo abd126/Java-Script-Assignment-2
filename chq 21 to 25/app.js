0d7afc58b8af5e234342e7204ed02a70f422c0e8
Abdullah <abdullahusman126@gmail.com>
2020-06-21 14:15:31 +0500

    Java Script Assginment

---
+++ chq 21 to 25/app.js
@@ -0,0 +1,46 @@
+// var firstName = prompt("Enter Your Name");
+// var lastName = prompt("Enter Your Last Name");
+// var fullName = firstName + " " + lastName ;
+// alert(fullName);
+
+// var favtMobile = prompt("Enter Your Favorite Mobile Model");
+// // alert("My Favorite Mobile is" + " " +favtMobile);
+// var userAnswar = ("My Favorite Mobile is" + " " + favtMobile);
+// alert(userAnswar);
+// alert("User String Count Words" + " " + userAnswar.length);
+
+// var str = "Pakistani";
+// var pos = str.indexOf("n");
+// alert(pos)
+
+// var str1 = "Hello World";
+// var pos1 = str1.lastIndexOf("l");
+// alert(pos1); 
+
+
+// var countrtyName = "Pakistani";
+// alert(countrtyName[3]);
+
+
+// var cities = ["Hyderabad"];
+// var rep = cities.replace(/Hydera/g , "Islama");
+// alert(rep)
+
+
+// var value = "472";
+// var value1 = +value;
+
+
+// var type = ("value :" + " " + value + "<br>" + "Type :" + " " + typeof value + "<br>" + "value :" + " " + value + "<br>" + "Type :" + " " + typeof value1 );
+// document.write(type)  
+
+// var userInput = prompt("Type Your Name");
+// var convert = userInput.toUpperCase();
+// alert(convert);
+
+var number = 35.36
+var changeType = number.toFixed();
+alert(changeType)
+
+
+
